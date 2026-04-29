d147a894052b07ca84506811471ca0aa8cfb9da0
devYuno <annykessy20@gmail,com>
2026-04-28 22:25:13 -0300

    FEAT: tela de login e cadastro

--- webreact/src/pages/login/login.js
+++ webreact/src/pages/login/login.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import '../../App.css';
+import { Link, useNavigate } from 'react-router-dom';
+import '../../App.css';
+
+// Gambiarra feia que eu fiz para navegar dentro de classe
+function withNavigation(Component) {
+  return (props) => {
+    const navigate = useNavigate();
+    return <Component {...props} navigate={navigate} />;
+  };
+}
 
 const credenciais = {
     emailReal: 'pucpr@gmail.com',
@@ -8,45 +18,50 @@ const credenciais = {
 
 class Login extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      senha: '',
-      message: ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            senha: '',
+            message: ''
+        }
     }
-  }
 
-  setEmail(event) {
-    let email = event.target.value
-    this.setState({ email: email })
-  }
-
-  setSenha(event) {
-    let senha = event.target.value
-    this.setState({ senha: senha })
-  }
-
-  acessar() {
-    if (this.state.email === credenciais.emailReal && this.state.senha === credenciais.senhaReal) {
-      this.setState({ message: "Acesso liberado" })
+    setEmail(event) {
+        let email = event.target.value
+        this.setState({ email: email })
     }
-    else {
-      this.setState({ message: "Acesso negado" })
-    }
-  }
 
-  render() {
-    return (
-      <div className='page'>
-        <h2>Login</h2>
-        <input id='input_email' name='email' type='text' onChange={(e) => { this.setEmail(e) }}></input>
-        <input id='input_senha' name='senha' type='text' onChange={(e) => { this.setSenha(e) }}></input>
-        <button id='btn_acessar' onClick={(e) => { this.acessar(e) }}>Acessar</button>
-        <span>{this.state.message}</span>
-      </div>
-    )
-  }
+    setSenha(event) {
+        let senha = event.target.value
+        this.setState({ senha: senha })
+    }
+
+    acessar() {
+        if (this.state.email === credenciais.emailReal && this.state.senha === credenciais.senhaReal) {
+            this.setState({ message: "Acesso liberado" })
+
+            this.props.navigate("/");
+        }
+        else {
+            this.setState({ message: "Acesso negado" })
+        }
+    }
+
+    render() {
+        return (
+            <div className='page auth'>
+                <div className='modal'>
+                    <h2>Login</h2>
+                    <input id='input_email' name='email' type='text' onChange={(e) => { this.setEmail(e) }}></input>
+                    <input id='input_senha' name='senha' type='password' onChange={(e) => { this.setSenha(e) }}></input>
+                    <button id='btn_acessar' onClick={(e) => { this.acessar(e) }}>Acessar</button>
+                    <Link className="link" to="/register">Não tenho cadastro</Link>
+                    <span className='message'>{this.state.message}</span>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Login;
+export default withNavigation(Login);
--- webreact/src/pages/register/register.js
+++ webreact/src/pages/register/register.js
@@ -1,10 +1,19 @@
 import '../../App.css'
+import { Link } from 'react-router-dom'
 
 function Register() {
     return (
-        <div className='page'>
-            
-        </div>
+        <div className='page auth'>
+                <div className='modal'>
+                    <h2>Cadastro</h2>
+                    <input id='input_name' name='name' type='text' onChange={(e) => {  }}></input>
+                    <input id='input_email' name='email' type='text' onChange={(e) => {  }}></input>
+                    <input id='input_senha' name='senha' type='password' onChange={(e) => {  }}></input>
+                    <button id='btn_acessar' onClick={(e) => {  }}>Acessar</button>
+                    <Link className="link" to="/login">Já tenho cadastro</Link>
+                    {/* <span className='message'>{this.state.message}</span> */}
+                </div>
+            </div>
     )
 }
 export default Register;
